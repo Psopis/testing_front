427ec5aeb7822284ef7068fcda2175bfb359fd42
psopi <konevvik05@mail.ru>
2024-11-13 05:02:10 +0700

    Working website 0.2.2 remade setUserId

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,31 @@
 import React from 'react';
 import SettingsMenu from './SettingsMenu'; // Импортируем компонент
 
-
+import {useEffect, useState} from "react"
 
 const exchanges = [
     {
         id: 'fl.ru',
         name: 'Fl',
-        logo: '/front/imges/fl.jpg',
+        logo: '/imges/fl.jpg',
         color: 'bg-blue-500'
     },
     {
         id: 'kwork',
         name: 'Kwork',
-        logo: '/front/imges/kwork.jpg',
+        logo: '/imges/kwork.jpg',
         color: 'bg-green-500'
     },
     {
         id: 'freelance.habr.com',
         name: 'Habr',
-        logo: '/front/imges/habr.jpg',
+        logo: '/imges/habr.jpg',
         color: 'bg-purple-500'
     },
     {
         id: 'freelance_ru',
         name: 'Freelance',
-        logo: '/front/imges/freelance.jpg',
+        logo: 'imges/freelance.jpg',
         color: 'bg-red-500'
     }
 ];
